22adf977f68f6c6daf0d2396728569dc6ea630ee
praydog <praydog@praydog.com>
2023-01-27 16:19:43 -0800

    Fix UGameEngine::Tick scan failing on obfuscated binaries

--- shared/sdk/UGameEngine.cpp
+++ shared/sdk/UGameEngine.cpp
@@ -18,19 +18,50 @@ std::optional<uintptr_t> UGameEngine::get_tick_address() {
             const auto engine = UEngine::get();
 
             if (engine != nullptr) {
-                const auto uengine_tick_pure_virtual = utility::find_virtual_function_from_string_ref(module, L"UEngine::Tick", true);
+                // Not find_virtual_function_from_string_ref because it can fail on obfuscated executables
+                // where the virtual func is actually a jmp wrapper to the real function that this string is in.
+                const auto uengine_tick_pure_virtual = utility::find_function_from_string_ref(module, L"UEngine::Tick", true);
 
                 if (!uengine_tick_pure_virtual) {
                     spdlog::error("Failed to find UEngine::Tick pure virtual");
                     return std::nullopt;
                 }
 
-                const auto uengine_tick_vtable_middle = utility::scan_ptr(module, *uengine_tick_pure_virtual);
+                spdlog::info("UEngine::Tick: {:x} (pure virtual)", (uintptr_t)*uengine_tick_pure_virtual);
 
+                auto uengine_tick_vtable_middle = utility::scan_ptr(module, *uengine_tick_pure_virtual);
+
+                // If this has happened then we are dealing with an obfuscated executable
+                // it's not the end of the world, we can still find the address
+                // through additional legwork.
                 if (!uengine_tick_vtable_middle) {
-                    spdlog::error("Failed to find UEngine::Tick vtable middle");
-                    return std::nullopt;
+                    uint32_t insn_size = 0;
+                    auto func_call = utility::scan_displacement_reference(module, *uengine_tick_pure_virtual);
+
+                    if (!func_call) {
+                        func_call = utility::scan_relative_reference_strict(module, *uengine_tick_pure_virtual, "E9"); // jmp
+
+                        if (!func_call) {
+                            spdlog::error("Failed to find UEngine::Tick vtable middle");
+                            return std::nullopt;
+                        }
+
+                        insn_size = 1;
+                    } else {
+                        insn_size = *func_call - utility::resolve_instruction(*func_call)->addr;
+                    }
+
+                    spdlog::info("UEngine::Tick reference (call or jmp): {:x}", (uintptr_t)*func_call);
+
+                    uengine_tick_vtable_middle = utility::scan_ptr(module, *func_call - insn_size);
+
+                    if (!uengine_tick_vtable_middle) {
+                        spdlog::error("Failed to find UEngine::Tick vtable middle even after finding a reference to it");
+                        return std::nullopt;
+                    }
                 }
+
+                spdlog::info("UEngine::Tick: {:x} (vtable middle)", (uintptr_t)*uengine_tick_vtable_middle);
 
                 // Keep going backwards through the vtable until we find an instruction that references the pointer
                 for (auto i = 1; i < 200; ++i) {
