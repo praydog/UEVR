f5811815d47e7ab9cff58e65d4e18455bb4c6b2a
praydog <praydog@praydog.com>
2023-10-25 18:42:22 -0700

    SDK: Various USceneComponent utilities

--- shared/sdk/USceneComponent.cpp
+++ shared/sdk/USceneComponent.cpp
@@ -114,4 +114,120 @@ void USceneComponent::set_world_location(const glm::vec3& location, bool sweep, 
 
     this->process_event(fn, params.data());
 }
+
+void USceneComponent::add_world_offset(const glm::vec3& location, bool sweep, bool teleport) {
+    static auto fn = static_class()->find_function(L"K2_AddWorldOffset");
+    static const auto fhitresult = sdk::find_uobject<UScriptStruct>(L"ScriptStruct /Script/Engine.HitResult");
+
+    if (fn == nullptr) {
+        return;
+    }
+
+    const auto fvector = sdk::ScriptVector::static_struct();
+    const auto is_ue5 = fvector->get_struct_size() == sizeof(glm::vec<3, double>);
+
+    // Need to dynamically allocate the params because of unknown FVector size
+    std::vector<uint8_t> params{};
+
+    // add a vec3
+    if (!is_ue5) {
+        params.insert(params.end(), (uint8_t*)&location, (uint8_t*)&location + sizeof(glm::vec3));
+    } else {
+        glm::vec<3, double> loc = location;
+        params.insert(params.end(), (uint8_t*)&loc, (uint8_t*)&loc + sizeof(glm::vec<3, double>));
+    }
+
+    // add a bool
+    params.insert(params.end(), (uint8_t*)&teleport, (uint8_t*)&sweep + sizeof(bool));
+    // align
+    params.insert(params.end(), 3, 0);
+    // add a FHitResult
+    params.insert(params.end(), fhitresult->get_struct_size(), 0);
+    // add a bool
+    params.insert(params.end(), (uint8_t*)&teleport, (uint8_t*)&teleport + sizeof(bool));
+
+    this->process_event(fn, params.data());
+}
+
+void USceneComponent::add_local_rotation(const glm::vec3& rotation, bool sweep, bool teleport) {
+    static auto fn = static_class()->find_function(L"K2_AddLocalRotation");
+    static const auto fhitresult = sdk::find_uobject<UScriptStruct>(L"ScriptStruct /Script/Engine.HitResult");
+
+    if (fn == nullptr) {
+        return;
+    }
+
+    const auto frotator = sdk::ScriptVector::static_struct();
+    const auto is_ue5 = frotator->get_struct_size() == sizeof(glm::vec<3, double>);
+
+    // Need to dynamically allocate the params because of unknown FRotator size
+    std::vector<uint8_t> params{};
+
+    // add a vec3
+    if (!is_ue5) {
+        params.insert(params.end(), (uint8_t*)&rotation, (uint8_t*)&rotation + sizeof(glm::vec3));
+    } else {
+        glm::vec<3, double> rot = rotation;
+        params.insert(params.end(), (uint8_t*)&rot, (uint8_t*)&rot + sizeof(glm::vec<3, double>));
+    }
+
+    // add a bool
+    params.insert(params.end(), (uint8_t*)&teleport, (uint8_t*)&sweep + sizeof(bool));
+    // align
+    params.insert(params.end(), 3, 0);
+    // add a FHitResult
+    params.insert(params.end(), fhitresult->get_struct_size(), 0);
+    // add a bool
+    params.insert(params.end(), (uint8_t*)&teleport, (uint8_t*)&teleport + sizeof(bool));
+
+    this->process_event(fn, params.data());
+}
+
+glm::vec3 USceneComponent::get_world_location() {
+    static const auto func = static_class()->find_function(L"K2_GetComponentLocation");
+    const auto fvector = sdk::ScriptVector::static_struct();
+
+    const auto is_ue5 = fvector->get_struct_size() == sizeof(glm::vec<3, double>);
+
+    std::vector<uint8_t> params{};
+
+    // add a vec3
+    if (!is_ue5) {
+        params.insert(params.end(), sizeof(glm::vec3), 0);
+    } else {
+        params.insert(params.end(), sizeof(glm::vec<3, double>), 0);
+    }
+
+    this->process_event(func, params.data());
+
+    if (!is_ue5) {
+        return *(glm::vec3*)params.data();
+    }
+        
+    return *(glm::vec<3, double>*)params.data();
+}
+
+glm::vec3 USceneComponent::get_world_rotation() {
+    static const auto func = static_class()->find_function(L"K2_GetComponentRotation");
+    const auto fvector = sdk::ScriptVector::static_struct();
+
+    const auto is_ue5 = fvector->get_struct_size() == sizeof(glm::vec<3, double>);
+
+    std::vector<uint8_t> params{};
+
+    // add a vec3
+    if (!is_ue5) {
+        params.insert(params.end(), sizeof(glm::vec3), 0);
+    } else {
+        params.insert(params.end(), sizeof(glm::vec<3, double>), 0);
+    }
+
+    this->process_event(func, params.data());
+
+    if (!is_ue5) {
+        return *(glm::vec3*)params.data();
+    }
+        
+    return *(glm::vec<3, double>*)params.data();
+}
 }
--- shared/sdk/USceneComponent.hpp
+++ shared/sdk/USceneComponent.hpp
@@ -14,5 +14,10 @@ public:
     void set_world_rotation(const glm::vec3& rotation, bool sweep = false, bool teleport = false);
     void add_world_rotation(const glm::vec3& rotation, bool sweep = false, bool teleport = false);
     void set_world_location(const glm::vec3& location, bool sweep = false, bool teleport = false);
+    void add_world_offset(const glm::vec3& offset, bool sweep = false, bool teleport = false);
+    void add_local_rotation(const glm::vec3& rotation, bool sweep = false, bool teleport = false);
+
+    glm::vec3 get_world_location();
+    glm::vec3 get_world_rotation();
 };
 }
