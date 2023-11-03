3f7369829ed7adf8ec6db8feff2e853211fe20e0
praydog <praydog@praydog.com>
2023-11-03 16:46:15 -0700

    SDK: Add APlayerCameraManager stub

---
+++ shared/sdk/APlayerCameraManager.cpp
@@ -0,0 +1,7 @@
+#include "APlayerCameraManager.hpp"
+
+namespace sdk {
+sdk::UClass* APlayerCameraManager::static_class() {
+    return (UClass*)sdk::find_uobject(L"Class /Script/Engine.PlayerCameraManager");
+}
+}
---
+++ shared/sdk/APlayerCameraManager.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "UObject.hpp"
+
+namespace sdk {
+class UClass;
+
+class APlayerCameraManager : public UObject {
+public:
+    static UClass* static_class();
+    // TODO
+};
+}
