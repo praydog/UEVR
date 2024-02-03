f4b8377de39d5b16ef8228fe253234ba1737f7b6
praydog <praydog@praydog.com>
2024-02-03 01:10:23 -0800

    Submodules: Update UESDK

--- src/CommitHash.hpp
+++ src/CommitHash.hpp
@@ -1,4 +1,4 @@
 #pragma once 
-#define UEVR_COMMIT_HASH "6e0f04ba1f79d3f14b0ec54495fe64b5e6b85018" 
+#define UEVR_COMMIT_HASH "6dcff35e794dcf31f4afad7ae49a38d1512cc207" 
 #define UEVR_BUILD_DATE "03.02.2024" 
 #define UEVR_BUILD_TIME "00:00" 
