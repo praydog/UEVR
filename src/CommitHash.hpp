bf9bb509be06233bf4d9ef5e05b7898955505d9a
praydog <praydog@praydog.com>
2024-02-05 13:14:17 -0800

    UI Compat: Expand scope to bespoke systems outside of AHUD This is achieved by hooking GetRenderTargetTexture There are some giveaways from where it's being called that we can use to return our UI render target instead of the scene

--- src/CommitHash.hpp
+++ src/CommitHash.hpp
@@ -1,4 +1,4 @@
 #pragma once 
-#define UEVR_COMMIT_HASH "18ed2b9982e9573bc6c9c5be4c99e354b1c8acdf" 
-#define UEVR_BUILD_DATE "04.02.2024" 
+#define UEVR_COMMIT_HASH "4c3aaf93799e9dbe4170f051cfc28915be6ad0fe" 
+#define UEVR_BUILD_DATE "05.02.2024" 
 #define UEVR_BUILD_TIME "00:00" 
