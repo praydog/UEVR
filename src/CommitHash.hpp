3997108934a41fdbb1c6a6e8ff59d7fcd63362ac
praydog <praydog@praydog.com>
2024-02-12 21:57:46 -0800

    Deps: Update kananlib (fix compile)

--- src/CommitHash.hpp
+++ src/CommitHash.hpp
@@ -1,4 +1,4 @@
 #pragma once 
-#define UEVR_COMMIT_HASH "68688a53bdaa9d5327e150469b499d059884e5cc" 
+#define UEVR_COMMIT_HASH "ef68b854db04abe74cf577f4d4a5a8c1a7e448fb" 
 #define UEVR_BUILD_DATE "12.02.2024" 
 #define UEVR_BUILD_TIME "00:00" 
