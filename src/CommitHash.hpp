b94f9bfaa31f80b5ebd49c092e5b89da6791e772
praydog <praydog@praydog.com>
2024-03-01 12:39:39 -0800

    Add PDBs to pure release builds without extra RelWithDebInfo sections

--- src/CommitHash.hpp
+++ src/CommitHash.hpp
@@ -1,4 +1,4 @@
 #pragma once 
-#define UEVR_COMMIT_HASH "52aca1a46d889a2fa3c4cab2af2964b0e8ec0e1c" 
-#define UEVR_BUILD_DATE "24.02.2024" 
+#define UEVR_COMMIT_HASH "1ea27b69c5965c56bb2fd43bf9a428fedda937a3" 
+#define UEVR_BUILD_DATE "01.03.2024" 
 #define UEVR_BUILD_TIME "00:00"
